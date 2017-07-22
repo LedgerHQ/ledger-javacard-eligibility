0311990018ad5522ba2e4488ebc82135315bbf71
Marcos Del Sol Vives <marcos@dracon.es>
2017-07-23 01:11:30 +0200

    Explicitely target 1.6

--- applet/build.sh
+++ applet/build.sh
@@ -5,7 +5,7 @@ cp src/com/ledger/eligibility/*.java build/src/com/ledger/eligibility
 cpp -P src/com/ledger/eligibility/Ripemd160.javap > build/src/com/ledger/eligibility/Ripemd160.java
 cpp -P src/com/ledger/eligibility/SHA512.javap > build/src/com/ledger/eligibility/SHA512.java
 cpp -P src/com/ledger/eligibility/LedgerEligibility.javap > build/src/com/ledger/eligibility/LedgerEligibility.java
-javac -g -classpath $JCENV/lib/api_classic.jar -sourcepath build/src -d build/classes build/src/com/ledger/eligibility/*.java 
+javac -g -source 1.6 -target 1.6 -classpath "$JCENV/lib/api_classic.jar" -sourcepath build/src -d build/classes build/src/com/ledger/eligibility/*.java
 java -classpath "$JCENV/lib/*" com.sun.javacard.converter.Main -exportpath "$JCENV/api_export_files" -useproxyclass -out CAP -classdir build/classes -d build/applet -applet 0xFF:0x4C:0x45:0x47:0x52:0x2E:0x45:0x4C:0x49:0x47:0x30:0x31:0x2E:0x49:0x30:0x31 com.ledger.eligibility.LedgerEligibility com.ledger.eligibility 0xFF:0x4C:0x45:0x47:0x52:0x2E:0x45:0x4C:0x49:0x47:0x30:0x31 1.0
 cp build/applet/com/ledger/eligibility/javacard/eligibility.cap build/Ledger-eligibility.cap 2>/dev/null
 rm -rf build/applet
